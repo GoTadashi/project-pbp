26290cad7d206189018f5ca09885b407be0f7d06
UncleJoo16 <jonathansatria16@gmail.com>
2023-12-04 11:24:13 +0700

    api guru

--- app/Http/Controllers/controller_api_guru.php
+++ app/Http/Controllers/controller_api_guru.php
@@ -4,58 +4,85 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\model_guru;
-use GuzzleHttp\Psr7\Message;
 
 class controller_api_guru extends Controller
 {
     public function getGuru()
     {
-        $query = model_guru::select('id_guru', 'nip', 'nama')->get();
-        return response()->json($query, 200, array(), JSON_PRETTY_PRINT);
+        try {
+            $query = model_guru::select('id_guru', 'nip', 'nama')->get();
+            return response()->json($query, 200, array(), JSON_PRETTY_PRINT);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'ERROR',
+                'message' => 'Gagal mengambil data guru: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function addGuru(Request $req)
     {
-        model_guru::create([
-            'nip' => $req->nip,
-            'nama' => $req->nama,
-            'tempat_lahir' => $req->tempat_lahir,
-            'tanggal_lahir' => $req->tanggal_lahir,
-            'jenis_kelamin' => $req->jenis_kelamin
-        ]);
+        try {
+            model_guru::create([
+                'nip' => $req->nip,
+                'nama' => $req->nama,
+                'tempat_lahir' => $req->tempat_lahir,
+                'tanggal_lahir' => $req->tanggal_lahir,
+                'jenis_kelamin' => $req->jenis_kelamin
+            ]);
 
-        return response()->json([
-            'status' => 'SUCCESS',
-            'message' => 'Data Berhasil Disimpan',
-        ], 200);
+            return response()->json([
+                'status' => 'SUCCESS',
+                'message' => 'Data Berhasil Disimpan',
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'ERROR',
+                'message' => 'Gagal menyimpan data guru: ' . $e->getMessage(),
+            ], 500);
+        }
     }
 
     public function deleteGuru(Request $req)
     {
-        $delete = model_guru::where('id_guru', $req->id)->delete();
-        if ($delete) {
+        try {
+            $delete = model_guru::where('id_guru', $req->id)->delete();
+            if ($delete) {
+                return response()->json([
+                    'status' => 'SUCCESS',
+                    'message' => 'Data Berhasil Dihapus',
+                ], 200);
+            }
+        } catch (\Exception $e) {
             return response()->json([
-                'status' => 'SUCCESS',
-                'message' => 'Data Berhasil Dihapus',
-            ], 200);
+                'status' => 'ERROR',
+                'message' => 'Gagal menghapus data guru: ' . $e->getMessage(),
+            ], 500);
         }
     }
 
     public function updateGuru(Request $req)
     {
-        $update = model_guru::where('id_guru', $req->id)->update([
-            'nip' => $req->nip,
-            'nama' => $req->nama,
-            'tempat_lahir' => $req->tempat_lahir,
-            'tanggal_lahir' => $req->tanggal_lahir,
-            'jenis_kelamin' => $req->jenis_kelamin,
-        ]);
+        try {
+            $update = model_guru::where('id_guru', $req->id)->update([
+                'nip' => $req->nip,
+                'nama' => $req->nama,
+                'tempat_lahir' => $req->tempat_lahir,
+                'tanggal_lahir' => $req->tanggal_lahir,
+                'jenis_kelamin' => $req->jenis_kelamin,
+            ]);
 
-        if ($update) {
+            if ($update) {
+                return response()->json([
+                    'status' => 'SUCCESS',
+                    'message' => 'Data Berhasil Diubah',
+                ], 200);
+            }
+        } catch (\Exception $e) {
             return response()->json([
-                'status' => 'SUCCESS',
-                'message' => 'Data Berhasil Diubah',
-            ], 200);
+                'status' => 'ERROR',
+                'message' => 'Gagal mengubah data guru: ' . $e->getMessage(),
+            ], 500);
         }
     }
 }
