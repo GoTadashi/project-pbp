faed22a913adc40198266caba3a0768779bf918a
GoTadashi <kevinat908@gmail.com>
2023-12-04 13:18:42 +0700

    asd

--- app/Http/Controllers/controller_api_raport.php
+++ app/Http/Controllers/controller_api_raport.php
@@ -18,12 +18,18 @@ class controller_api_raport extends Controller
                 'raport.id_siswa',
                 'siswa.nama as nama_siswa',
                 'raport.id_guru',
-                'guru.nama as nama_guru'
+                'guru.nama as nama_guru',
+                'matapelajaran.nama_matapelajaran',
+                'detail_raport.nilai',
+                'detail_raport.predikat',
+                'detail_raport.deskripsi'
             )
-            ->join('siswa', 'siswa.nis', '=', 'raport.id_siswa')
-            ->join('guru', 'guru.id_guru', '=', 'raport.id_guru')
-            ->get();
-    
+                ->join('siswa', 'siswa.nis', '=', 'raport.id_siswa')
+                ->join('guru', 'guru.id_guru', '=', 'raport.id_guru')
+                ->join('detail_raport', 'raport.id_raport', '=', 'detail_raport.id_raport')
+                ->join('matapelajaran', 'matapelajaran.id_matapelajaran', '=', 'detail_raport.id_matapelajaran')
+                ->get();
+
             return response()->json($raports, 200);
         } catch (\Exception $e) {
             return response()->json([
@@ -32,7 +38,7 @@ class controller_api_raport extends Controller
             ], 500);
         }
     }
-    
+
 
     public function getDetailRaport()
     {
