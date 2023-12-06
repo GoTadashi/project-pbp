0ecaf974f6ff6fd95fc151f089287d2851c2d27f
GoTadashi <kevinat908@gmail.com>
2023-12-07 03:05:03 +0700

    trycatch login

--- app/Http/Controllers/controller_api_login.php
+++ app/Http/Controllers/controller_api_login.php
@@ -9,15 +9,19 @@ class controller_api_login extends Controller
 {
     public function actionLogin(Request $request)
     {
-        $data = [
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-        ];
+        try {
+            $data = [
+                'email' => $request->input('email'),
+                'password' => $request->input('password'),
+            ];
 
-        if (Auth::attempt($data)) {
-            return response()->json(['success' => 'Login successful'], 200);
-        } else {
-            return response()->json(['error' => 'Invalid email or password'], 401);
+            if (Auth::attempt($data)) {
+                return response()->json(['success' => 'Login successful'], 200);
+            } else {
+                return response()->json(['error' => 'Invalid email or password'], 401);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Login failed: ' . $e->getMessage()], 500);
         }
     }
 }
