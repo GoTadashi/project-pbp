cdd2ff0f85cca04cfdab13fca5426f6a4987a151
GoTadashi <kevinat908@gmail.com>
2023-12-06 23:45:24 +0700

    .

--- app/Http/Controllers/controller_api_register.php
+++ app/Http/Controllers/controller_api_register.php
@@ -10,24 +10,31 @@ class controller_api_register extends Controller
 {
     public function actionRegister(Request $request)
     {
-        // Validasi
-        $request->validate([
-            'email' => 'required|email|unique:users',
-            'name' => 'required|unique:users',
-            'password' => 'required|min:6',
-            'role' => 'required',
-        ]);
+        try {
+            // Validasi
+            $request->validate([
+                'email' => 'required|email|unique:users',
+                'name' => 'required|unique:users',
+                'password' => 'required|min:6',
+                'role' => 'required',
+            ]);
 
-        // Buat pengguna baru
-        $user = User::create([
-            'name' => $request->name,
-            'password' => Hash::make($request->password),
-            'role' => $request->role,
-            'email' => $request->email,
-            'active' => 1
-        ]);
+            // Buat pengguna baru
+            $user = User::create([
+                'name' => $request->name,
+                'password' => Hash::make($request->password),
+                'role' => $request->role,
+                'email' => $request->email,
+                'active' => 1
+            ]);
 
-        // Return JSON response
-        return response()->json(['message' => 'Registration successful']);
+            // Return JSON response for successful registration
+            return response()->json(['message' => 'Registration successful']);
+        } catch (\Exception $e) {
+            // Tangani pengecualian di sini, misalnya:
+            return response()->json([
+                'message' => 'Registration failed: ' . $e->getMessage()
+            ], 500); // Status code 500 untuk kesalahan server
+        }
     }
 }
